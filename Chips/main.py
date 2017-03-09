6ad468d1e0e6885c673de9e19aa0bb330d04638d
Lucas Fijen <lucas.fijen@gmail.com>
2017-03-09 20:41:22 +0100

    fixed visualisation

--- Chips/main.py
+++ Chips/main.py
@@ -2,15 +2,25 @@ from __future__ import print_function
 
 def initialise():
     # Width, length, height
-    grid = [[[0 for i in range(6)] for j in range(7)] for k in range(2)]
+    grid = [[[0 for i in range(2)] for j in range(7)] for k in range(7)]
 
     with open("coordinates_test.txt") as f:
         for line in f.read().split():
-            name = line.split(",")
-            x = line.split(",")
-            y = line.split(",")
+            array = line.split(",")
+            name = array[0]
+            x = int(array[1])
+            y = int(array[2])
+            print(x, y)
             grid[x][y][0] = name
 
-    print(grid)
+    printgrid(grid, 0)
+    print('')
+    printgrid(grid, 1)
+
+def printgrid(grid, z):
+    for j in range (len(grid)):
+        for i in range (len(grid[0])):
+            print(grid[j][i][z], end=' ')
+        print('')
 
 initialise()
