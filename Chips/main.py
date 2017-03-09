57c8b9db7049bfe15fa5205990e7ae509cdb19a1
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2017-03-09 20:29:14 +0100

    initialise

--- Chips/main.py
+++ Chips/main.py
@@ -1,16 +1,16 @@
+from __future__ import print_function
+
 def initialise():
     # Width, length, height
-    grid = [18][13][7]
+    grid = [[[0 for i in range(6)] for j in range(7)] for k in range(2)]
 
+    with open("coordinates_test.txt") as f:
+        for line in f.read().split():
+            name = line.split(",")
+            x = line.split(",")
+            y = line.split(",")
+            grid[x][y][0] = name
 
-
-
-
-
-
-
-
-
-
+    print(grid)
 
 initialise()
