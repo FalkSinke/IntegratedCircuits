8a89a97ce56b79ceb52bf141c610590138e03740
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2017-03-16 19:12:06 +0100

    options

--- Chips/main.py
+++ Chips/main.py
@@ -1,8 +1,12 @@
 from __future__ import print_function
 
+x_max = 6
+y_max = 6
+z_max = 1
+
 def initialise():
     # Width, length, height
-    grid = [[["." for i in range(2)] for j in range(7)] for k in range(7)]
+    grid = [[["." for i in range(z_max+1)] for j in range(x_max+1)] for k in range(y_max+1)]
 
     with open("coordinates_test.txt") as f:
         for line in f.read().split():
@@ -17,10 +21,35 @@ def initialise():
     print('')
     printgrid(grid, 1)
 
+    options(grid, 1, 0, 0)
+
+
 def printgrid(grid, z):
     for j in range (len(grid)):
         for i in range (len(grid[0])):
             print(grid[j][i][z], end=' ')
         print('')
 
+def options(grid, x, y, z):
+    options = []
+
+    if x != x_max and grid[x+1][y][z] == '.':
+        options.append([x+1, y, z])
+    if x!= 0 and grid[x - 1][y][z] == '.':
+        options.append([x - 1, y, z])
+    if y != y_max and grid[x][y+1][z] == '.':
+        options.append([x, y+1, z])
+    if y != 0 and grid[x][y-1][z]:
+        options.append([x, y-1, z])
+    if z != z_max and grid[x][y][z+1] == '.':
+        options.append([x, y, z+1])
+    if z != 0 and grid[x][y][z-1] == '.':
+        options.append([x, y, z-1])
+
+    print(options)
+
+    return options
+
+
 initialise()
+
