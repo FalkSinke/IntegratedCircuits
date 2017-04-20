08e955bd1f459d9f94d052a81537d05eb8aa5cbd
ClaartjeBarkof <claartjebarkhof@hotmail.com>
2017-04-20 20:48:55 +0200

    penalty heat map

--- Chips/main.py
+++ Chips/main.py
@@ -18,11 +18,14 @@ def main():
     init = initialise()
     grid = init[0]
     points = init[1]
+    penalty_grid = initialise_penalty_grid(points)
+    printgrid(penalty_grid, 0)
+    printgrid(grid, 0)
     with open("netlist_1.txt") as netlist:
         counter = 0
         for line in netlist.read().split():
             array = line.split(",")
-            printpath(grid, a_star(grid, points[array[0]], points[array[1]]), '*')
+            printpath(grid, a_star(grid, penalty_grid, points[array[0]], points[array[1]]), '*')
             counter = counter + 1
             printgrid(grid, 0)
             printgrid(grid, 1)
@@ -57,6 +60,7 @@ def printgrid(grid, z):
         print('')
     print('')
 
+'''
 def get_penalty_grid_point(grid, options):
     penalised_options = []
 
@@ -79,26 +83,56 @@ def get_penalty_grid_point(grid, options):
         penalised_options.append([[x, y, z], counter])
 
     return penalised_options
+'''
 
-def options(grid, point):
+'''
+penalty grid DOCUMENTEREN
+    - heat varieren
+    - penalty functie varieren (linear/kwadratisch etc)
+    - proberen met andere netlists/grids
+
+
+uberhaupt documenteren van Penalty
+
+resultaten tabel: alleen a*, met penalty (verschillende soorten), hillclimber?
+
+'''
+
+def initialise_penalty_grid(points_dict):
+    penalty_grid = [[[0 for i in range(z_max+1)] for j in range(y_max+1)] for k in range(x_max+1)]
+    heat = 10
+
+    for x in range(len(penalty_grid)):
+        for y in range(len(penalty_grid[x])):
+            for z in range(len(penalty_grid[x][y])):
+                for gate in points_dict:
+                    distance = calc_admissable([x,y,z], points_dict[gate])
+                    penalty = heat - (distance*2);
+                    if penalty < 0:
+                        penalty = 0
+                    penalty_grid[x][y][z] += penalty
+
+    return penalty_grid
+
+def options(grid, penalty_grid, point):
     options = []
 
     x, y, z = point
 
     if x != x_max and grid[x+1][y][z] == '.':
-        options.append([x+1, y, z])
-    if x!= 0 and grid[x - 1][y][z] == '.':
-        options.append([x - 1, y, z])
+       options.append([[x+1, y, z], penalty_grid[x+1][y][z]])
+    if x!= 0 and grid[x-1][y][z] == '.':
+        options.append([[x-1, y, z], penalty_grid[x-1][y][z]])
     if y != y_max and grid[x][y+1][z] == '.':
-        options.append([x, y+1, z])
+        options.append([[y+1, y, z], penalty_grid[y+1][y][z]])
     if y != 0 and grid[x][y-1][z] == '.':
-        options.append([x, y-1, z])
+        options.append([[y-1, y, z], penalty_grid[y-1][y][z]])
     if z != z_max and grid[x][y][z+1] == '.':
-        options.append([x, y, z+1])
+        options.append([[z+1, y, z], penalty_grid[z+1][y][z]])
     if z != 0 and grid[x][y][z-1] == '.':
-        options.append([x, y, z-1])
+        options.append([[z-1, y, z], penalty_grid[z-1][y][z]])
 
-    return get_penalty_grid_point(grid, options)
+    return options
 
 #Calculates total path length from A to B going by point n + 1
 def calc_admissable(a, b):
@@ -118,7 +152,7 @@ als je snelste route van a naar b wil vinden:
 
 '''
 
-def a_star(grid, a, b):
+def a_star(grid, penalty_grid, a, b):
     prioq = Q.PriorityQueue()
     admissable = calc_admissable(a, b)
     visited = []
@@ -131,7 +165,7 @@ def a_star(grid, a, b):
             print(current_path)
             return current_path
 
-        possible = options(grid, current_path[-1])
+        possible = options(grid, penalty_grid, current_path[-1])
         for i, k in possible:
             if (i not in visited):
                 visited.append(i)
